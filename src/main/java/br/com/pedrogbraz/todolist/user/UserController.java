018ee49413da8f451742a5d00a470e4f1ae91f63
pedrogbraz <pedrobrazdevs@gmail.com>
2024-05-24 13:42:54 -0300

    feat: add repository to database

--- src/main/java/br/com/pedrogbraz/todolist/user/UserController.java
+++ src/main/java/br/com/pedrogbraz/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.pedrogbraz.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired // Definindo que o spring que vai gerenciar o ciclo de vida
+    private IUserRepository userRepository;
+
     /*
      * String (texto)
      * Integer (int) - Números inteiros
@@ -25,7 +29,8 @@ public class UserController {
      * void (void) - Quando não possui nenhum retorno
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
